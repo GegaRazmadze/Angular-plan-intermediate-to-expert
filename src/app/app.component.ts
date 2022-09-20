b9b4fc029bd0d2ba0c5fdd02e0641f2fbfa5e235
GegaRazmadze <razmadzegega@gmail.com>
2022-09-20 13:24:26 +0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Angular-plan-intermediate-to-expert';
+}
